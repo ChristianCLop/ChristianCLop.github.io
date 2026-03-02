c45dbb34837d5694f3a81d3fa0c658622754c057
Christian Chico <93953404+ChristianCLop@users.noreply.github.com>
2026-03-02 11:40:56 -0500

    Constants Experience

---
+++ src/constants/experiences.ts
@@ -0,0 +1,62 @@
+export const experiences = [
+  {
+    img: "https://media.licdn.com/dms/image/v2/D4E0BAQHSvvoRbwD4nw/company-logo_200_200/company-logo_200_200/0/1690086602887?e=1773878400&v=beta&t=iFPbd4UtuE_KeFqiimcWn1MjuKx9QLr_pnEMilVFlMs",
+    title: "Software Engineer",
+    company: "SanTIC Education",
+    date: "December 2025 - January 2026",
+    item1:
+      "Design and implementation of Docker-based architecture (Nginx + PHP + Moodle + PostgreSQL).",
+    item2: "Server configuration and administration on AlmaLinux.",
+    item3: "Docker and Docker Compose configuration and management.",
+    item4:
+      "Reverse proxy implementation with SSL certificates (Let’s Encrypt) for multiple instances.",
+    item5:
+      "Automation of cron jobs and background tasks for provisioning new instances and managing backups.",
+    skills: [
+      { name: "PHP", img: "/icons/skills/php.svg" },
+      { name: "Docker", img: "/icons/skills/docker.svg" },
+      { name: "AlmaLinux", img: "/icons/skills/almalinux.svg" },
+      { name: "PostgreSQL", img: "/icons/skills/postgresql.svg" },
+    ],
+  },
+  {
+    img: "/img/experience/UTA.webp",
+    img2: "/img/experience/FISEI.png",
+    title: "Laboratory Technician",
+    company: "Universidad Técnica de Ambato - FISEI",
+    date: "September 2025 - January 2026",
+    item1: "Server administration and maintenance.",
+    item2:
+      "Management, support, and maintenance of internal faculty applications.",
+    item3: "Technical support and maintenance of academic IT infrastructure.",
+    item4:
+      "Development of web applications and internal tools to optimize processes.",
+    skills: [
+      { name: "Rocky Linux", img: "/icons/skills/rocky_linux.svg" },
+      { name: "Docker", img: "/icons/skills/docker.svg" },
+    ],
+  },
+  {
+    img: "/img/experience/UTA.webp",
+    img2: "/img/experience/FISEI.png",
+    title: "Laboratory Intern",
+    company: "Universidad Técnica de Ambato - FISEI",
+    date: "February 2024 - July 2024",
+    item1:
+      "Development of a web application to manage laboratory, subject and teacher schedules.",
+    item2: "Development of a mobile application for ID card validator.",
+    item3: "Maintenance of a web application for document management.",
+    item4: "Machine maintenance and support.",
+    item5: "Maintenance and restructuring of classrooms.",
+    skills: [
+      { name: "React Native", img: "/icons/skills/reactjs.svg" },
+      { name: "Angular", img: "/icons/skills/angular.svg" },
+      { name: "MySQL", img: "/icons/skills/mysql.svg" },
+      { name: "JavaScript", img: "/icons/skills/javascript.svg" },
+      {
+        name: "Technical Support",
+        img: "/icons/skills/technical_support.svg",
+      },
+    ],
+  },
+];
